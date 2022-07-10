99226ac267c0bf09cf5b32d3591856c5844ee861
yallurium <christos@dartling.dev>
2022-07-10 20:15:45 +0200

    Add iOS support with Swift

--- ios/Classes/SwiftAppUsagePlugin.swift
+++ ios/Classes/SwiftAppUsagePlugin.swift
@@ -1,14 +1,28 @@
 import Flutter
 import UIKit
 
-public class SwiftAppUsagePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "app_usage", binaryMessenger: registrar.messenger())
-    let instance = SwiftAppUsagePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+public class SwiftAppUsagePlugin: NSObject, FlutterPlugin, AppUsageApi {
+    var usedApps = [
+        UsedApp(id: "com.reddit.app", name: "Reddit", minutesUsed: 75),
+        UsedApp(id: "dev.hashnode.app", name: "Hashnode", minutesUsed:37),
+        UsedApp(id: "link.timelog.app", name: "Timelog", minutesUsed: 25)
+    ]
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let messenger : FlutterBinaryMessenger = registrar.messenger()
+        let api : AppUsageApi & NSObjectProtocol = SwiftAppUsagePlugin.init()
+        AppUsageApiSetup.setUp(binaryMessenger: messenger, api: api)
+    }
+
+    func getPlatformVersion(completion: @escaping (String?) -> Void) {
+        completion("iOS " + UIDevice.current.systemVersion)
+    }
+
+    func getApps(completion: @escaping ([UsedApp]) -> Void) {
+        completion(usedApps)
+    }
+
+    func setAppTimeLimit(appId: String, durationInMinutes: Int32, completion: @escaping (TimeLimitResult) -> Void) {
+        completion(TimeLimitResult(state: ResultState.success, message: "Timer of \(durationInMinutes) minutes set for app ID \(appId)"))
+    }
 }
