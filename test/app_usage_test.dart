19d69c8f6957c1745d3e76e2ffba8ac09e61de86
yallurium <christos@dartling.dev>
2022-07-10 15:58:00 +0200

    Update tests

--- test/app_usage_test.dart
+++ test/app_usage_test.dart
@@ -1,29 +1,61 @@
+import 'package:app_usage/app_usage_api.dart';
+import 'package:app_usage/app_usage_pigeon.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:app_usage/app_usage.dart';
 import 'package:app_usage/app_usage_platform_interface.dart';
-import 'package:app_usage/app_usage_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-class MockAppUsagePlatform 
+class MockAppUsagePlatform
     with MockPlatformInterfaceMixin
     implements AppUsagePlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<List<UsedApp>> get apps async =>
+      [UsedApp(id: "id", name: "name", minutesUsed: 60)];
+
+  @override
+  Future<TimeLimitResult> setAppTimeLimit(
+      String appId, Duration duration) async {
+    return TimeLimitResult(state: ResultState.success, message: "Success");
+  }
 }
 
 void main() {
   final AppUsagePlatform initialPlatform = AppUsagePlatform.instance;
 
-  test('$MethodChannelAppUsage is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelAppUsage>());
+  test('$PigeonAppUsage is the default instance', () {
+    expect(initialPlatform, isInstanceOf<PigeonAppUsage>());
   });
 
   test('getPlatformVersion', () async {
     AppUsage appUsagePlugin = AppUsage();
     MockAppUsagePlatform fakePlatform = MockAppUsagePlatform();
     AppUsagePlatform.instance = fakePlatform;
-  
+
     expect(await appUsagePlugin.getPlatformVersion(), '42');
+  });
+
+  test('getPlatformVersion', () async {
+    AppUsage appUsagePlugin = AppUsage();
+    MockAppUsagePlatform fakePlatform = MockAppUsagePlatform();
+    AppUsagePlatform.instance = fakePlatform;
+
+    final apps = await appUsagePlugin.apps;
+    expect(apps.length, 1);
+    expect(apps.first.id, "id");
+    expect(apps.first.name, "name");
+    expect(apps.first.minutesUsed, 60);
+  });
+
+  test('getPlatformVersion', () async {
+    AppUsage appUsagePlugin = AppUsage();
+    MockAppUsagePlatform fakePlatform = MockAppUsagePlatform();
+    AppUsagePlatform.instance = fakePlatform;
+
+    final result = await appUsagePlugin.setAppTimeLimit("id", Duration.zero);
+    expect(result.state, ResultState.success);
+    expect(result.message, "Success");
   });
 }
